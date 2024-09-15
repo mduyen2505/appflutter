bf119d09fc18150867f71833a384697d3a398ef8
duytran1508 <ngocduyxx1508@gmail.com>
2024-09-15 21:35:57 +0700

    Tạo API đăng ký người dùng ở backend

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,30 @@
-const json = require("body-parser/lib/types/json")
+// const json = require("body-parser/lib/types/json")
 const UserService = require('../services/UserService') 
+// const { status } = require("express/lib/response")
 
 const createUser = async (req, res) => {
     try{
-        console.log(req.body)
-       const res = await UserService.createUser()
-        return res.status(200).json(res)
+        const {name, email, password, confirmPassword, phone, access_token , refresh_token } = req.body 
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
+        if (!name || !email || !password || !confirmPassword || !phone){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'the input is required'
+            })
+        }else if(!isCheckEmail){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'the input is email'
+            })
+        }else if(password !== confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'the password is equal confirmPassword '
+            })
+        }
+       const response = await UserService.createUser(req.body)
+        return res.status(200).json(response)
     }catch(e){
         return res.status(404).json({
             message: e
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,37 @@
-const createUser = () =>{
-    return new Promise((resolve, reject) => {
+// const { status } = require('express/lib/response')
+const User = require('../models/UserModel')
+
+const createUser = (newUser) =>{
+    return new Promise(async (resolve, reject) => {
+        const {name, email, password, confirmPassword, phone, access_token, refresh_token} = newUser
+
         try {
-            resolve({})
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if (checkUser != null){
+                resolse({
+                    status: 'Oke',
+                    message: 'Email is already'
+                })
+            }
+            const createdUser = await User.create({
+                name, 
+                email, 
+                password, 
+                confirmPassword, 
+                phone,
+                access_token,
+                refresh_token
+            })
+            if(createdUser){
+
+                resolve({
+                    status: 'Oke',
+                    massage: 'Success',
+                    data: createdUser
+                })
+            }
         }catch(e){
             reject(e)
         }
