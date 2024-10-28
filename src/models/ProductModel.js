3d11c827d827aebcce15a53de54731d3637bbc29
duytran1508 <ngocduyxx1508@gmail.com>
2024-10-28 21:31:53 +0700

    sửa product

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -3,19 +3,19 @@ const timespan = require("jsonwebtoken/lib/timespan");
 const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    productsTypeName: { type: String, required: true },
-    quantityInStock: { type: Number, required: true },
-    prices: { type: Number, required: true },
-    inches: { type: Number, required: true },
-    screenResolution: { type: String, required: true },
+    productsTypeName: { type: String},
+    quantityInStock: { type: Number},
+    prices: { type: Number, required: true},
+    inches: { type: Number},
+    screenResolution: { type: String},
     imageUrl: { type: String, required: true },
-    bannerUrl: { type: String, required: true },
+    bannerUrl: { type: String},
     company: { type: String, required: true },
-    cpu: { type: String, required: true },
-    ram: { type: String, required: true },
-    memory: { type: String, required: true },  
-    gpu: { type: String, required: true },
-    weight: { type: Number, required: true }
+    cpu: { type: String},
+    ram: { type: String},
+    memory: { type: String},  
+    gpu: { type: String},
+    weight: { type: Number}
 }, 
     { timestamps: true });
 
